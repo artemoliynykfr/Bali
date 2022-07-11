9f006b01577ba3afb2da26e5fbdf4738cc91eb86
Artem Oliynyk <oliynykartemfr@gmail.com>
2022-07-11 11:17:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,47 +9,46 @@ function testWebP(callback) {
 testWebP(function (support) {
    if (support == true) {
       document.querySelector('body').classList.add('webp');
-   }else{
+   } else {
       document.querySelector('body').classList.add('no-webp');
    }
 });
 // header burger	
-function burgerMenu(){
-	const burger = document.querySelector('.burger')
-	const menu = document.querySelector('.menu')
-	const body = document.querySelector('body')
-	burger.addEventListener('click', () => { 
-		if(!menu.classList.contains('active')){
-			menu.classList.add('active')
-			burger.classList.add('active-burger')
-			body.classList.add('locked')
-		} else {
-			menu.classList.remove('active')
-			burger.classList.remove('active-burger')
-			body.classList.remove('locked')
-		}
-	})
-	window.addEventListener('resize', () => {
-		if (window.innerWidth > 991.98){
-			menu.classList.remove('active')
-			burger.classList.remove('active-burger')
-			body.classList.remove('locked')
-		}
-	})
+function burgerMenu() {
+   const burger = document.querySelector('.burger')
+   const menu = document.querySelector('.menu')
+   const body = document.querySelector('body')
+   burger.addEventListener('click', () => {
+      if (!menu.classList.contains('active')) {
+         menu.classList.add('active')
+         burger.classList.add('active-burger')
+         body.classList.add('locked')
+      } else {
+         menu.classList.remove('active')
+         burger.classList.remove('active-burger')
+         body.classList.remove('locked')
+      }
+   })
+   window.addEventListener('resize', () => {
+      if (window.innerWidth > 991.98) {
+         menu.classList.remove('active')
+         burger.classList.remove('active-burger')
+         body.classList.remove('locked')
+      }
+   })
 }
 burgerMenu();
-function fixedNav(){
-	const nav = document.querySelector('nav')
-	const breakpoint = 1
-	if (window.scrollY > breakpoint){
-		nav.classList.add('fixed__nav')
-	} else {
-		nav.classList.remove('fixed__nav')
-	}
+function fixedNav() {
+   const nav = document.querySelector('nav')
+   const breakpoint = 1
+   if (window.scrollY > breakpoint) {
+      nav.classList.add('fixed__nav')
+   } else {
+      nav.classList.remove('fixed__nav')
+   }
 }
 window.addEventListener('scroll', fixedNav)
-
-
+//
 var x, i, j, l, ll, selElmnt, a, b, c;
 x = document.getElementsByClassName('select');
 l = x.length;
@@ -65,7 +64,7 @@ for (i = 0; i < l; i++) {
    for (j = 1; j < ll; j++) {
       c = document.createElement('DIV');
       c.innerHTML = selElmnt.options[j].innerHTML;
-      c.addEventListener('click', function(e) {
+      c.addEventListener('click', function (e) {
          var y, i, k, s, h, sl, yl;
          s = this.parentNode.parentNode.getElementsByTagName('select')[0];
          sl = s.length;
@@ -88,7 +87,7 @@ for (i = 0; i < l; i++) {
       b.appendChild(c);
    }
    x[i].appendChild(b);
-   a.addEventListener('click', function(e) {
+   a.addEventListener('click', function (e) {
       e.stopPropagation();
       closeAllSelect(this);
       this.nextSibling.classList.toggle('select-hide');
@@ -115,8 +114,7 @@ function closeAllSelect(elmnt) {
    }
 }
 document.addEventListener('click', closeAllSelect);
-
-
+//
 new Swiper('.places__swiper > .swiper-container', {
    direction: 'horizontal',
    loop: !0,
@@ -131,17 +129,17 @@ new Swiper('.places__swiper > .swiper-container', {
       disableOnInteraction: true,
    },
    coverflowEffect: {
-     rotate: 0,
-     stretch: 0,
-     depth: 50,
-     modifier: 1,
-     slideShadows: true,
+      rotate: 0,
+      stretch: 0,
+      depth: 50,
+      modifier: 1,
+      slideShadows: true,
    },
    breakpoint: {
-      993:{
+      993: {
          slidesPerView: "auto",
       },
-      481:{
+      481: {
          slidesPerView: 2,
          coverflowEffect: {
             rotate: 0,
@@ -149,9 +147,9 @@ new Swiper('.places__swiper > .swiper-container', {
             depth: 10,
             modifier: 1,
             slideShadows: true,
-          },
+         },
       },
-      0:{
+      0: {
          slidesPerView: 1,
       },
    },
@@ -160,18 +158,19 @@ new Swiper('.places__swiper > .swiper-container', {
       onlyInViewport: !1
    },
 });
+//
 new Swiper(".partners__swiper > .swiper-container", {
    direction: "horizontal",
-   loop: true, 
+   loop: true,
    spaceBetween: 46,
    slidesPerView: "auto",
-   loop: true,
    autoplay: {
       delay: 0,
       disableOnInteraction: false
    },
-   speed: 5000,
- });
+   speed: 4000,
+});
+//
 new Swiper('.customers__swiper > .swiper-container', {
    direction: 'horizontal',
    loop: !0,
@@ -187,70 +186,4 @@ new Swiper('.customers__swiper > .swiper-container', {
       nextEl: '.swiper-next',
       prevEl: '.swiper-prev',
    },
-});
-const tl = new TimelineMax()
-tl.from('nav', {y: "-100%", duration: .8})
-gsap.registerPlugin(ScrollTrigger);
-gsap.utils.toArray('.animation__opacity').forEach((el, index) => {
-   let tl = gsap.timeline({
-      scrollTrigger: {
-         trigger: el,
-         start: 'top bottom',
-         toggleActions: 'play none none reverse',
-      }
-   })
-   tl
-      .set(el, {transformOrigin: 'center center'})
-      .fromTo(el, {opacity: 0}, {opacity: 1, duration: 1, immediateRender: false})
-})
-gsap.utils.toArray('.animation__top').forEach((el, index) => {
-   let tl = gsap.timeline({
-      scrollTrigger: {
-         trigger: el,
-         start: 'top bottom',
-         toggleActions: 'play none none reverse',
-      }
-   })
-   tl
-      .set(el, {transformOrigin: 'center center'})
-      .fromTo(el, {y: "-10%"}, {y: "0%", duration: 1, immediateRender: false})
-})
-gsap.utils.toArray('.animation__left').forEach((el, index) => {
-   let tl = gsap.timeline({
-      scrollTrigger: {
-         trigger: el,
-         start: 'top bottom',
-         toggleActions: 'play none none reverse',
-      }
-   })
-   tl
-      .set(el, {transformOrigin: 'center center'})
-      .fromTo(el, {x: "-10%"}, {x: "0%", duration: 1, immediateRender: false})
-})
-gsap.utils.toArray('.animation__right').forEach((el, index) => {
-   let tl = gsap.timeline({
-      scrollTrigger: {
-         trigger: el,
-         start: 'top bottom',
-         toggleActions: 'play none none reverse',
-      }
-   })
-   tl
-      .set(el, {transformOrigin: 'center center'})
-      .fromTo(el, {x: "10%"}, {x: "0%", duration: 1, immediateRender: false})
-})
-gsap.utils.toArray('.animation__bottom').forEach((el, index) => {
-   let tl = gsap.timeline({
-      scrollTrigger: {
-         trigger: el,
-         start: 'top bottom',
-         toggleActions: 'play none none reverse',
-      }
-   })
-   tl
-      .set(el, {transformOrigin: 'center center'})
-      .fromTo(el, {y: "30%"}, {y: "0%", duration: 1, immediateRender: false})
-})
-let scroll = new SmoothScroll('a[href*="#"]', {
-	speed: 600
 });
